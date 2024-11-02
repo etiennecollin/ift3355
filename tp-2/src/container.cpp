a3fc743f843b3d0ee28fdaf087c513a7fb60daf6
etiennecollin <collin.etienne.contact@gmail.com>
2024-11-01 22:01:22 -0400

    Cleanup

--- tp-2/src/container.cpp
+++ tp-2/src/container.cpp
@@ -17,21 +17,21 @@ bool BVH::intersect(Ray ray, double t_min, double t_max, Intersection* hit) { re
 // - Retourner l'intersection avec la profondeur maximale la plus PETITE.
 bool Naive::intersect(Ray ray, double t_min, double t_max, Intersection* hit) {
     // Iterate through the objects
+    std::vector aabbs = Naive::aabbs;
     std::vector objects = Naive::objects;
     bool did_hit = false;
 
     for (int i = 0; i < objects.size(); i++) {
-        AABB aabb = Naive::aabbs[i];
         Intersection local_hit;
         // Detect intersection with AABB
-        if (aabb.intersect(ray, t_min, t_max)) {
+        if (aabbs[i].intersect(ray, t_min, t_max)) {
             // Detect intersection with geometry
             if (objects[i]->intersect(ray, t_min, t_max, &local_hit)) {
                 // Only update hit if it is the first hit or if it is closer
-                // than the previous hit and is not behind the camera
-                if ((i == 0 || local_hit.depth < hit->depth) && local_hit.depth >= 0) {
+                // than the previous closest hit
+                if (i == 0 || local_hit.depth < hit->depth) {
                     did_hit = true;
-                    hit = &local_hit;
+                    *hit = local_hit;
                 }
             }
         }
