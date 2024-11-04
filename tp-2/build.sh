ba65c5a7e9e856f1f13d63ecb449b17d2b7669ff
etiennecollin <collin.etienne.contact@gmail.com>
2024-11-04 14:39:40 -0500

    Updated script

--- tp-2/build.sh
+++ tp-2/build.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Create the build directory if it doesn't exist
 mkdir -p build
 
+# Check if -r flag was passed to enable logging
+RELEASE=false
+if [ "$1" == "-r" ]; then
+    echo "Building with release profile"
+    RELEASE=true
+else
+    echo "Building with debug profile"
+fi
+
 # Run cmake with the provided target
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build
+if $RELEASE; then
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O1" -S . -B build
+else
+    cmake -DCMAKE_BUILD_TYPE=Debug -S . -B build
+fi
 
 # Check if cmake ran successfully
 if [ $? -eq 0 ]; then
--- tp-2/render_all.sh
+++ tp-2/render_all.sh
@@ -1,13 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Check that fd and parallel are installed
-if [ ! -x "$(command -v fd)" ]; then
-    echo "fd is not installed. Please install it."
+# Function to display an error message and exit
+function error_exit {
+    echo "$1" >&2
     exit 1
+}
+
+# Check that fd is installed
+if [ ! -x "$(command -v fd)" ]; then
+    error_exit "Error: 'fd' is not installed. Please install it."
 fi
 
-mkdir -p logs
+# Check if -l flag was passed to enable logging
+LOGGING=false
+if [ "$1" == "-l" ]; then
+    echo "Logging enabled."
+    echo "Logs will be saved in ./logs/ directory"
+    mkdir -p logs
+    LOGGING=true
+fi
 
 # Render in parallel
-# time fd -e ray -p ./data/scene -x sh -c "./build/RAY '{/}' > './logs/{/.}.txt'"
-time fd -e ray -p ./data/scene -x ./build/RAY "{/}"
+if $LOGGING; then
+    time fd -e ray -d 1 "." ./data/scene -x sh -c "./build/RAY '{/}' > './logs/{/.}.txt'"
+else
+
+    time fd -e ray -d 1 "." ./data/scene -x ./build/RAY "{/}"
+fi
