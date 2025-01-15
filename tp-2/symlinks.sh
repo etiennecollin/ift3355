7b303cc3d56a9dbb523f5563630a6728fcdf1001
etiennecollin <collin.etienne.contact@gmail.com>
2025-01-14 19:43:39 -0500

    Fixed symlinks

---
+++ tp-2/symlinks.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Create a symlink to the data directory in the zig-out/bin/ directory
+mkdir -p ./zig-out/bin/
+cd ./zig-out/bin/
+ln -s ../../data/ ./data
+echo "Created symlink: data directory is now available to the executable"
+echo "If you encounter an error saying the scene files cannot be found when executing the raytracer, run this script again"
